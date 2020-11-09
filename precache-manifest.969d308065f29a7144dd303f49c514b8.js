3aa51a073809533259ec19714bd7a790be03bfbd
Mauricio Palacios <mauriciopalacio1990@gmail.com>
2020-11-08 22:44:37 -0600

    Updates

---
+++ precache-manifest.969d308065f29a7144dd303f49c514b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6cc186da3ca24139ec3be443949de85",
+    "url": "./index.html"
+  },
+  {
+    "revision": "983f96a28dda38049857",
+    "url": "./static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ed9d64656cea7af053b3",
+    "url": "./static/css/main.822c7ac6.chunk.css"
+  },
+  {
+    "revision": "983f96a28dda38049857",
+    "url": "./static/js/2.4aeb276e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "./static/js/2.4aeb276e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed9d64656cea7af053b3",
+    "url": "./static/js/main.d0da350d.chunk.js"
+  },
+  {
+    "revision": "d949b06110a71d877761",
+    "url": "./static/js/runtime-main.aef88a7a.js"
+  }
+]);
